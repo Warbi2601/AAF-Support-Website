68f8fe13e53446171e4ec0494898e2238a0828e4
Warbi2601 <joshwarbi@hotmail.com>
2021-03-02 18:04:37 +0000

    added new routes for individual ticket actions

--- backend/routes/ticket.routes.js
+++ backend/routes/ticket.routes.js
@@ -13,7 +13,28 @@ router.post(
   controller.addTicket
 );
 
+// Open Ticket on behalf // Will be POST
+
 router.put("/", controller.updateTicket);
+
+//Client
+router.put("/reopen-ticket", controller.updateTicket);
+router.put("/add-information", controller.updateTicket);
+router.put("/close-ticket", controller.updateTicket);
+router.put("/cancel-ticket-user", controller.updateTicket);
+
+//Support
+router.put("/allocate-ticket-self", controller.updateTicket);
+router.put("/check-ticket", controller.updateTicket);
+router.put("/reallocate-ticket", controller.updateTicket);
+router.put("/solve-ticket", controller.updateTicket);
+router.put("/suspend-ticket", controller.updateTicket);
+router.put("/cancel-ticket-support", controller.updateTicket);
+router.put("/cancel-abandoned-ticket", controller.updateTicket);
+
+//Admin
+router.put("/allocate-ticket-support", controller.updateTicket);
+router.put("/close-ticket-expired", controller.updateTicket);
 
 // router.delete("/", controller.deleteTicket);
 
